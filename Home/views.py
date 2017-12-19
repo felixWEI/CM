fb88c7f3f3653edeeefb51de4e5eaafbd11108c8
DESKTOP-3KPB3FE\felixWei <helloweifan@qq.com>
2017-12-19 21:48:28 +0800

    update

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
-@login_required
-def home(request):
+def home_page(request):
     return render(request, 'index.html')
